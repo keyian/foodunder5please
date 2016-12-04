7404ba9ff72651e3605be86c99b91f94f48efb76
keyian <keyian@nyu.edu>
2016-12-04 14:20:35 -0500

    possible homepage problem

--- start-client.js
+++ start-client.js
@@ -1,3 +1,4 @@
 const args = [ 'start' ];
 const opts = { stdio: 'inherit', cwd: 'projClient', shell: true };
+console.log("we go to start client still");
 require('child_process').spawn('npm', args, opts);
