a43714566fb4992ee5d7b0768a0c9810feef7f81
keyian <keyian@nyu.edu>
2016-12-04 14:34:47 -0500

    switch const to var

--- start-client.js
+++ start-client.js
@@ -1,9 +1,9 @@
 console.log("we go to start client still");
 console.log(process.env.NODE_ENV);
 if(process.env.NODE_ENV === "PRODUCTION") {
-  const args = [ 'build' ];
+  var args = [ 'build' ];
 } else {
-  const args = [ 'start' ];
+  var args = [ 'start' ];
 }
 
 const opts = { stdio: 'inherit', cwd: 'projClient', shell: true };
