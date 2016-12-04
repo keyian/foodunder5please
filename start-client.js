d0f3dc6fafddd13bf2c852127fecdf2b5913545e
keyian <keyian@nyu.edu>
2016-12-04 14:32:28 -0500

    perhap sstartclient was the culprit...

--- start-client.js
+++ start-client.js
@@ -1,4 +1,10 @@
-const args = [ 'start' ];
-const opts = { stdio: 'inherit', cwd: 'projClient', shell: true };
 console.log("we go to start client still");
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === "PRODUCTION") {
+  const args = [ 'build' ];
+} else {
+  const args = [ 'start' ];
+}
+
+const opts = { stdio: 'inherit', cwd: 'projClient', shell: true };
 require('child_process').spawn('npm', args, opts);
