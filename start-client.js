48c3b9b518023f8c87b497fe69cd874c65c8b28b
keyian <keyian@nyu.edu>
2016-12-04 14:47:17 -0500

    perhaps my misunderstanding lies in what a build actually is. maybe i just need to not even run anything on the client, just serve those statics. build taken off gitignore for this reason...

--- start-client.js
+++ start-client.js
@@ -2,7 +2,6 @@ if(process.env.NODE_ENV === "PRODUCTION") {
   var args = [ 'build' ];
 } else {
   var args = [ 'start' ];
+  const opts = { stdio: 'inherit', cwd: 'projClient', shell: true };
+  require('child_process').spawn('npm', args, opts);
 }
-
-const opts = { stdio: 'inherit', cwd: 'projClient', shell: true };
-require('child_process').spawn('npm', args, opts);
